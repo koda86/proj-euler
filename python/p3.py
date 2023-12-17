55da50b79222dcb2c71cab3dbed3f299ef9b32c7
koda86 <78311630+koda86@users.noreply.github.com>
2023-12-17 01:23:31 +0100

    Update p3.py

--- python/p3.py
+++ python/p3.py
@@ -3,15 +3,21 @@ Problem 3: The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143?
 
-Daniel Koska (koda86), Dec 17 2023
+Daniel Koska (koda86), Dec 13 2021
 """
 
 # Prime factors: 5*7*13*29 = 13195
 
 # Check if number is prime
 def is_prime(n):
-  for i in range(10):
-    if n % i == 3:
-      print("test")
+  for i in range(2, n-1):
+    if n%i == 0:
+      print(i)
+      print("No prime number!")
+      return False
     else:
-      print("test1")
+      print(i)
+      print("Is a prime number!")
+      return True
+
+is_prime(16)
